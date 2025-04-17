c05930f2321082ef9ef503c5c5a4fb9aba3b0790
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-04-17 06:49:24 +0000

    update: add support combine separate composite keys

--- src/spark_utils.py
+++ src/spark_utils.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.connect.dataframe import DataFrame as ConnectDataFrame
+import pyspark.sql.functions as spf
 
 
 def table_exists(spark_session: SparkSession, path: str) -> bool:
@@ -124,3 +125,71 @@ def create_schema(spark_session: SparkSession, path: str, if_not_exists: bool = 
     spark_session.sql(
         f"create schema {'if not exists' if if_not_exists else ''} {path}"
     )
+
+
+def combine_composite_key(
+    spark_session: SparkSession,
+    df: DataFrame | ConnectDataFrame,
+    key_cols: list[str],
+    composite_key_col: str,
+) -> DataFrame | ConnectDataFrame:
+    """
+    Add a composite key column to the DataFrame.
+
+    Args:
+        spark_session (SparkSession): The Spark session.
+        df (DataFrame | ConnectDataFrame): The DataFrame to add the composite key to.
+        key_cols (list): The list of columns to use for the composite key.
+        composite_key_col (str): The name of the composite key column.
+
+    Returns:
+        DataFrame | ConnectDataFrame: The DataFrame with the composite key column added.
+    """
+
+    return df.withColumn(composite_key_col, spf.concat_ws("_", *key_cols))
+
+
+def separate_composite_key(
+    spark_session: SparkSession,
+    df: DataFrame | ConnectDataFrame,
+    composite_key_col: str,
+    key_cols: list[str],
+) -> DataFrame | ConnectDataFrame:
+    """
+    Separate a composite key column into individual columns.
+
+    Args:
+        spark_session (SparkSession): The Spark session.
+        df (DataFrame | ConnectDataFrame): The DataFrame to separate the composite key from.
+        composite_key_col (str): The name of the composite key column.
+        key_cols (list): The list of columns to use for the composite key.
+
+    Returns:
+        DataFrame | ConnectDataFrame: The DataFrame with the composite key column separated.
+    """
+
+    return df.withColumn(
+        composite_key_col, spf.split(df[composite_key_col], "_").alias(key_cols)
+    )
+
+
+def get_unique_col_name(cols: list[str], name_hint: str) -> str:
+    """
+    Generate a unique column name based on the provided list of columns and a name hint.
+
+    Args:
+        cols (list): The list of existing column names.
+        name_hint (str): The hint for the new column name.
+
+    Returns:
+        str: A unique column name.
+    """
+
+    if name_hint not in cols:
+        return name_hint
+
+    i = 1
+    while f"{name_hint}_{i}" in cols:
+        i += 1
+
+    return f"{name_hint}_{i}"
